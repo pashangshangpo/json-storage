408fd37575e7fdc20c5616083592b0f75fceca4c
pashangshangpo <pashangshangpo@gamil.com>
2018-06-29 19:08:37 +0800

    基本逻辑

--- index.js
+++ index.js
@@ -4,3 +4,40 @@
  * @date 2018-06-29 18:24:44
  */
 
+const fs = require('fs')
+
+class Storage {
+    constructor(root) {
+        this.root = root
+
+        if (!this.has(this.root)) {
+            this.root.split('/').map((item, index, list) => {
+                const filePath = list.slice(0, index + 1).join('/')
+
+                if (filePath && !this.has(filePath)) {
+                    fs.mkdirSync(filePath)
+                }
+            })
+        }
+    }
+
+    has(key) {
+        return fs.existsSync(key)
+    }
+
+    set(key, data) {
+        
+    }
+
+    get(key) {
+
+    }
+
+    getKeys() {
+
+    }
+
+    find(key) {
+
+    }
+}
